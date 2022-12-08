0efdd004998f71dd350fafd4606333d159f554c3
Laércio Asano <laercio.asano@gmail.com>
2022-12-08 12:55:27 -0300

    Refactoring

--- dpk.js
+++ dpk.js
@@ -1,28 +1,32 @@
 const crypto = require("crypto");
 
+function genHash(value) {
+  return crypto.createHash("sha3-512").update(value).digest("hex");
+}
+
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
-
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  // Using a guard reduces nesting and makes reading the rest of the function without caring
+  // about this false values.
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  // Starting with the non existing partitionKey express more clearly what should be done in this case,
+  // which doesn't have multiple steps as it would seem by the original code.
+  if (!event.partitionKey) {
+    return genHash(JSON.stringify(event));
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+
+  // Now it is clear that the provided value has to pass a few verifications and be adjusted accordingly.
+  // Verifying if the provided value is a string and stringifying otherwise.
+  const candidate = typeof event.partitionKey === "string" 
+      ? event.partitionKey 
+      : JSON.stringify(event.partitionKey);
+
+  // Verifying if the provided value is of the correct lenght and hashing otherwise.
+  return candidate.length <= MAX_PARTITION_KEY_LENGTH 
+      ? candidate 
+      : genHash(candidate);
 };
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,84 @@
+const crypto = require("crypto");
 const { deterministicPartitionKey } = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
+  });
+
+  it("Returns the literal '0' when given false values as input", () => {
+    const values =  ["", 0, false];
+    for (const v of values) {
+      const result = deterministicPartitionKey(v);
+      expect(result).toBe("0");
+    }
+  });
+
+  it("Returns hash of strigification for values that don't have a .partitionKey field", () => {
+    const values =  [[], {}];
+    for(let i = 0; i < 300; i++) {
+      values[0].push(i);
+      values[1][i] = i;
+    }
+    for (const v of values) {
+      const result = deterministicPartitionKey(v);
+      const expected = crypto.createHash("sha3-512").update(JSON.stringify(v)).digest("hex");
+      expect(result).toBe(expected);
+    }
+  });
+
+
+  it("Returns the .partitionKey field if it contains a valid value", () => {
+    let maxLenString = "";
+    for(var i = 0; i < 256; i++) {
+      maxLenString += "a";
+    }
+
+    const values =  [{partitionKey: "asdfsad"}, {partitionKey: "a", x: 2}, {partitionKey: maxLenString, y: 1, z: 3}];
+    for (const v of values) {
+      const result = deterministicPartitionKey(v);
+      expect(result).toBe(v.partitionKey);
+    }
+  });
+
+  it("Returns the hashed value of .partitionKey field if it is a string larger than 256", () => {
+    let overValue1 = "";
+    let overValue2 = "";
+    for(var i = 0; i < 257; i++) {
+      overValue1 += "a";
+      overValue2 += "a";
+      overValue2 += "b";
+    }
+
+    const values =  [{partitionKey: overValue1}, {partitionKey: overValue2, x: 2}];
+    for (const v of values) {
+      const result = deterministicPartitionKey(v);
+      const expected = crypto.createHash("sha3-512").update(v.partitionKey).digest("hex");
+      expect(result).toBe(expected);
+    }
+  });
+
+  it("Returns stringified value of .partitionKey field if it is not a string and stringifies for less than 256 characters", () => {
+    const values =  [{partitionKey: 321}, {partitionKey: {1: 3}, x: 2}];
+    for (const v of values) {
+      const result = deterministicPartitionKey(v);
+      const expected = JSON.stringify(v.partitionKey);
+      expect(result).toBe(expected);
+    }
+  });
+
+  it("Returns stringified hashed value of .partitionKey field if it is not a string and stringifies for more than 256 characters", () => {
+    const large_keys =  [[], {}];
+    for(let i = 0; i < 300; i++) {
+      large_keys[0].push(i);
+      large_keys[1][i] = i;
+    }
+    const values =  [{partitionKey: large_keys[0]}, {partitionKey: large_keys[1], x: 2}];
+    for (const v of values) {
+      const result = deterministicPartitionKey(v);
+      const expected = crypto.createHash("sha3-512").update(JSON.stringify(v.partitionKey)).digest("hex");
+      expect(result).toBe(expected);
+    }
   });
 });
